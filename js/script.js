0a6b0e33ad28dc60ffa5509d7ea90cb991caa693
Liz <51245710+eahowell@users.noreply.github.com>
2024-01-22 20:05:07 -0600

    Update: X and delete function to jQuery

--- js/script.js
+++ js/script.js
@@ -18,15 +18,14 @@ function newItem() {
   });
 
   //3(i). Adding the delete button "X":
-  let crossOutButton = document.createElement("crossOutButton");
-  crossOutButton.appendChild(document.createTextNode("X"));
-  li.appendChild(crossOutButton);
+  let crossOutButton = $(`<crossOutButton>X</crossOutButton>`);
+  li.append(crossOutButton);
 
-  crossOutButton.addEventListener("click", deleteListItem);
   //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
-  function deleteListItem() {
-    li.classList.add("delete");
-  }
+  crossOutButton.on("click", function () {
+    li.addClass("delete");
+  });
+
   // 4. Reordering the items:
   $("#list").sortable();
 }
